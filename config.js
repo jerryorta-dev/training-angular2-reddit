b4d8581b389e178ede2d5a6e92f0d6bc0850c3ee
Orta, Jerry <Jerry.Orta@schwab.com>
2015-08-10 15:45:42 -0500

    final app

---
+++ config.js
@@ -0,0 +1,24 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "traceur",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "lodash-node": "npm:lodash-node@3.10.1",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:lodash-node@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
